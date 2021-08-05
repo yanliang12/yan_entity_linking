0316b125f5e4562d95a474d3d1f4675bd899fe07
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-08-05 22:27:33 +0400

    Add files via upload

--- yan_entity_linking_example.py
+++ yan_entity_linking_example.py
@@ -8,7 +8,11 @@ for e in o:
 	print(e)
 
 '''
-{'entity_wikipage_id': '3956428', 'sentence': 'I live at Abu Dhabi but study in Heriot-Watt University Dubai.', 'mention': 'heriot watt university dubai'}
-{'entity_wikipage_id': '18950756', 'sentence': 'I live at Abu Dhabi but study in Heriot-Watt University Dubai.', 'mention': 'abu dhabi'}
+
+{'mention': 'heriot watt university dubai', 'entity_wikipage_id': '3956428', 'sentence': 'I study at Heriot-Watt University Dubai, but I live at Abu Dhabi. I want to work at Apple. I was born in China, 1997'}
+{'mention': 'china', 'entity_wikipage_id': '5405', 'sentence': 'I study at Heriot-Watt University Dubai, but I live at Abu Dhabi. I want to work at Apple. I was born in China, 1997'}
+{'mention': 'abu dhabi', 'entity_wikipage_id': '18950756', 'sentence': 'I study at Heriot-Watt University Dubai, but I live at Abu Dhabi. I want to work at Apple. I was born in China, 1997'}
+{'mention': 'apple', 'entity_wikipage_id': '856', 'sentence': 'I study at Heriot-Watt University Dubai, but I live at Abu Dhabi. I want to work at Apple. I was born in China, 1997'}
+
 ''' 
 #######yan_entity_linking_example.py########
